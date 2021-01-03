3fdb28bc0f5bf889429bec0e187c8457bbe55c1f
gunerhanale <gunerhanale@gmail.com>
2021-01-03 17:37:52 +0100

    ApplicationContext,SpringContainer,singletonVsPrototype

---
+++ SpringDemo/src/main/java/com/ebubekir/demo/Alien.java
@@ -0,0 +1,15 @@
+package com.ebubekir.demo;
+
+public class Alien {
+
+	int age;
+	
+	public Alien() {
+		System.out.println("Alien object created..");
+	}
+
+	public void code() {
+		System.out.println("Im coding..");
+	}
+
+}
