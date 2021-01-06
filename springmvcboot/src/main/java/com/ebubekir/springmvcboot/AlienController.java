e92199d75466bd298bca7c2fb108ba848084d721
gunerhanale <gunerhanale@gmail.com>
2021-01-06 23:35:03 +0100

    created Rest API AlienController GET request

---
+++ springmvcboot/src/main/java/com/ebubekir/springmvcboot/AlienController.java
@@ -0,0 +1,26 @@
+package com.ebubekir.springmvcboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ebubekir.springmvcboot.model.Alien;
+
+@Controller
+public class AlienController {
+
+	@Autowired
+	AlienRepo repo;
+
+	@GetMapping("aliens")
+	@ResponseBody
+	public List<Alien> getAliens() {
+
+		List<Alien> aliens = repo.findAll();
+		return aliens;
+	}
+
+}
